5fd8a04670f9f9ca626091dc60a9ac71123fb939
dorian-marchal <marchal.dorian@gmail.com>
2015-02-11 07:57:59 +0100

    Renomme les models de test

--- rest-server.js
+++ rest-server.js
@@ -16,32 +16,32 @@ mongoose.model('MyModel', MyModelSchema);
 var MyModel = mongoose.model('MyModel');
 
 // This function is responsible for returning all entries for the Message model
-function getMessages(req, res, next) {
+function getMyModels(req, res, next) {
   // Resitify currently has a bug which doesn't allow you to set default headers
   // This headers comply with CORS and allow us to server our response to any origin
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   // .find() without any arguments, will return all results
   // the `-1` in .sort() means descending order
-  Message.find().sort('date', -1).execFind(function (arr,data) {
+  MyModel.find().sort('date', -1).execFind(function (arr,data) {
     res.send(data);
   });
 }
 
 
 
-function postMessage(req, res, next) {
+function postMyModel(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  // Create a new message model, fill it up and save it to Mongodb
-  var message = new Message();
-  message.message = req.params.message;
-  message.date = new Date();
-  message.save(function () {
+  // Create a new MyModel model, fill it up and save it to Mongodb
+  var mymodel = new MyModel();
+  mymodel.message = req.params.message;
+  mymodel.date = new Date();
+  mymodel.save(function () {
     res.send(req.body);
   });
 }
 
 // Set up our routes and start the server
-server.get('/messages', getMessages);
-server.post('/messages', postMessage);
+server.get('/mymodels', getMyModels);
+server.post('/mymodels', postMyModel);
