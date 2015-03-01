9ba9b0a2162a0c6381af928f5f194dcc1f140757
dorian-marchal <marchal.dorian@gmail.com>
2015-03-01 22:53:48 +0100

    Move the specific code in the `server` folder

--- core/RestServer.js
+++ core/RestServer.js
@@ -22,77 +22,21 @@ RestServer.prototype.start = function(onStart, onError) {
 
     console.log('Database connection...');
 
-    var mongoose = require('mongoose/');
+    that.mongoose = require('mongoose/');
 
-    that.db = mongoose.connection;
-    mongoose.connect(that.config.db.auth);
+    that.db = that.mongoose.connection;
+    that.mongoose.connect(that.config.db.auth);
 
     that.db.on('error', onError);
 
     that.db.once('open', function () {
 
         console.log('Database connected !');
-        onStart();
-
-        var MyModelSchema = new mongoose.Schema({
-            attribute: String,
-            attribute2: Number,
-            date: Date
-        });
-
-        mongoose.model('MyModel', MyModelSchema);
-        var MyModel = mongoose.model('MyModel');
-
-        function getMyModels(req, res) {
-
-            MyModel
-                .find()
-                .sort({
-                    date: 'desc'
-                })
-                .exec(function (err, data) {
-                    if (err) {
-                        return console.error(err);
-                    }
-                    res.send(data);
-                })
-            ;
-        }
-
-        function postMyModel(req, res) {
-
-            var mymodel = new MyModel();
-
-            if (req.params.attribute) {
-                mymodel.attribute = req.params.attribute;
-            }
-            else {
-                mymodel.attribute = 'default attribute';
-            }
-
-            if (req.params.attribute2) {
-                mymodel.attribute2 = req.params.attribute2;
-            }
-            else {
-                mymodel.attribute2 = -1;
-            }
-
-            mymodel.date = new Date();
-
-            mymodel.save(function (err) {
-                if (err) {
-                    return console.error(err);
-                }
-                res.send(req.body);
-            });
-        }
 
         that.router.listen(that.config.port, function() {
             console.log('Server listening on port ' + that.config.port + '...');
+            onStart();
         });
-        // Set up our routes and start the server
-        that.router.get('/mymodels', getMyModels);
-        that.router.post('/mymodels', postMyModel);
 
     });
 };
