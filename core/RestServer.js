da79b371d48bdceb991771d865859ebc61c8ed04
dorian-marchal <marchal.dorian@gmail.com>
2015-03-28 19:07:00 +0100

    Replace LocalStrategy by BearerStrategy

--- core/RestServer.js
+++ core/RestServer.js
@@ -10,7 +10,7 @@
  * Server Options :
  *     useAuth (default: false) ; If true, the passport property will be defined
  *         this.passport : passport instance
- *         this.LocalStrategy : passport LocalStrategy
+ *         this.BearerStrategy : passport BearerStrategy (token based strategy)
  *     useMongo (default : false) ; If true, mongoConnection and mongoose will be defined
  *         this.mongoose : Mongoose instance
  *         this.mongoConnection : MongoDb connection
@@ -45,19 +45,11 @@ var RestServer = function(options) {
     // Configure passport authentification
     if (options.useAuth) {
         var passport = require('passport');
-        var sessions = require('express-session');
 
-        router.use(sessions({
-            secret: this.config.sessionSecret,
-            maxAge: null,
-            resave: true,
-            saveUninitialized: true,
-        }));
         router.use(passport.initialize());
-        router.use(passport.session());
 
         this.passport = passport;
-        this.LocalStrategy = require('passport-local').Strategy;
+        this.BearerStrategy = require('passport-http-bearer').Strategy;
     }
 
     this.router = router;
