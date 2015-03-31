af69cdfc53b30de7da2271a78942f220894f943c
Dorian MARCHAL <marchal.dorian@gmail.com>
2015-03-31 17:33:52 +0200

    Add a base server

---
+++ server/server.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var RestServer = require('../core/RestServer');
+
+var server = new RestServer({
+    useAuth: false,
+    useMongo: false,
+    useMysql: false,
+});
+
+var onStart = function() {
+
+    server.router.get('/', function(req, res) {
+        res.send('Yup. It works.');
+    });
+}
+
+server.start(onStart);
