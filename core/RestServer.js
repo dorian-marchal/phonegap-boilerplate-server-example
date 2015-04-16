84d671dccc2c30754172da3575eb0ac9ee9967e2
dorian-marchal <marchal.dorian@gmail.com>
2015-04-17 00:23:09 +0200

    Change server db instances
    - Remove server.mongoConnection
    - move server.mongoose, server.mysql and server.mysqlConnection in
      express instance

--- core/RestServer.js
+++ core/RestServer.js
@@ -9,11 +9,10 @@
  *
  * Server Options :
  *     useMongo (default : false) ; If true, mongoConnection and mongoose will be defined
- *         this.mongoose : Mongoose instance
- *         this.mongoConnection : MongoDb connection
+ *         this.app.set('mongoose') : Mongoose instance
  *     useMysql (default : false) ; If true, mysqlConnection will be defined
- *         that.mysql : Mysql module instance
- *         that.mysqlConnection : MySQL connection
+ *         that.app.set('mysql') : Mysql module instance
+ *         that.app.set('mysqlConnection') : MySQL connection
  */
 var RestServer = function(options) {
 
@@ -58,16 +57,19 @@ RestServer.prototype.start = function(onStart) {
     // Make a connection to the mongo database
     var connectMongo = function(callback) {
         console.log('MongoDB connection...');
-        that.mongoose = require('mongoose/');
-        that.mongoConnection = that.mongoose.connection;
-        that.mongoose.connect(that.config.db.mongo.auth);
 
-        that.mongoConnection.on('error', function(err) {
+        var mongoose = require('mongoose/');
+        var connection = mongoose.connection;
+        mongoose.connect(that.config.db.mongo.auth);
+
+        that.app.set('mongoose', mongoose);
+
+        connection.on('error', function(err) {
             console.error('MongoDB error');
             throw err;
         });
 
-        that.mongoConnection.once('open', function () {
+        connection.once('open', function () {
             console.log('MongoDB connected !');
             callback(null);
         });
@@ -76,16 +78,20 @@ RestServer.prototype.start = function(onStart) {
     // Make a connection to the mysql database
     var connectMysql = function(callback) {
         console.log('MySQL connection...');
-        that.mysql = require('mysql');
 
-        that.mysqlConnection = that.mysql.createConnection({
+        var mysql = require('mysql');
+
+        var mysqlConnection = that.mysql.createConnection({
             host : that.config.db.mysql.host,
             user : that.config.db.mysql.username,
             password : that.config.db.mysql.password,
             database : that.config.db.mysql.database,
         });
 
-        that.mysqlConnection.connect(function(err) {
+        that.app.set('mysql', mysql);
+        that.app.set('mysqlConnection', mysqlConnection);
+
+        mysqlConnection.connect(function(err) {
             if (err) {
                 console.error('MySQL error');
                 throw err;
