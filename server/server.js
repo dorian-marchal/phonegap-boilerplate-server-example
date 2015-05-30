07d57ec2a47bd6b41951dbfe0d767af671927b47
dorian-marchal <marchal.dorian@gmail.com>
2015-05-30 23:48:15 +0200

    Wrap mongodb auth and add an option to choose the db

--- server/server.js
+++ server/server.js
@@ -11,127 +11,136 @@ auth.addTo(server);
 
 function onStart() {
 
-    var mongoose = server.app.get('mongoose');
+    var mongoAuth = function () {
 
-    // Configure the authentification
-    var UserSchema = new mongoose.Schema({
-        token: String,
-        username: String,
-        password: String,
-    }, {
-        collection: 'User',
-    });
+        var mongoose = server.app.get('mongoose');
 
+        // Configure the authentification
+        var UserSchema = new mongoose.Schema({
+            token: String,
+            username: String,
+            password: String,
+        }, {
+            collection: 'User',
+        });
 
-    var User = mongoose.model('User', UserSchema);
+        var User = mongoose.model('User', UserSchema);
 
-    auth.findUserByToken = function(token, done) {
+        auth.findUserByToken = function(token, done) {
 
-        User.findOne(
-            {
-                token: token
-            },
-            function(err, user) {
+            User.findOne(
+                {
+                    token: token
+                },
+                function(err, user) {
+                    if (err) {
+                        return done(err);
+                    }
+                    if (!user) {
+                        return done('Incorrect token.');
+                    }
+                    return done(null, user);
+                }
+            );
+        };
+
+        auth.findUserByUsernameAndPassword = function(username, password, done) {
+
+            User.findOne(
+                {
+                    username: username,
+                    password: password,
+                },
+                function(err, user) {
+
+                    if (err) {
+                        return done(err);
+                    }
+                    if (!user) {
+                        return done();
+                    }
+
+                    done(null, user);
+                }
+            );
+        };
+
+        auth.updateUserToken = function(user, token, done) {
+
+            user.token = token;
+
+            user.save(function(err) {
                 if (err) {
                     return done(err);
                 }
-                if (!user) {
-                    return done('Incorrect token.');
-                }
-                return done(null, user);
-            }
-        );
-    };
+                done();
+            });
+        };
 
-    auth.findUserByUsernameAndPassword = function(username, password, done) {
+        var MyModelSchema = new mongoose.Schema({
+            attribute: String,
+            attribute2: Number,
+            date: Date
+        });
 
-        User.findOne(
-            {
-                username: username,
-                password: password,
+        mongoose.model('MyModel', MyModelSchema);
+        var MyModel = mongoose.model('MyModel');
+
+        return {
+            getMyModels: function (req, res) {
+
+                MyModel
+                    .find()
+                    .sort({
+                        date: 'desc'
+                    })
+                    .exec(function (err, data) {
+                        if (err) {
+                            return console.error(err);
+                        }
+                        res.send(data);
+                    })
+                ;
             },
-            function(err, user) {
 
-                if (err) {
-                    return done(err);
+            postMyModel: function(req, res) {
+
+                var mymodel = new MyModel();
+
+                if (req.body.attribute) {
+                    mymodel.attribute = req.body.attribute;
                 }
-                if (!user) {
-                    return done();
+                else {
+                    mymodel.attribute = 'default attribute';
                 }
 
-                done(null, user);
-            }
-        );
+                if (req.body.attribute2) {
+                    mymodel.attribute2 = req.body.attribute2;
+                }
+                else {
+                    mymodel.attribute2 = -1;
+                }
+
+                mymodel.date = new Date();
+
+                mymodel.save(function (err) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    res.send(req.body);
+                });
+            },
+        };
     };
 
-    auth.updateUserToken = function(user, token, done) {
+    var mysqlAuth = function () {
 
-        user.token = token;
-
-        user.save(function(err) {
-            if (err) {
-                return done(err);
-            }
-            done();
-        });
     };
 
+    var authData = server.config.databaseEngine === 'mysql' ? mysqlAuth() : mongoAuth();
 
-    var MyModelSchema = new mongoose.Schema({
-        attribute: String,
-        attribute2: Number,
-        date: Date
-    });
-
-    mongoose.model('MyModel', MyModelSchema);
-    var MyModel = mongoose.model('MyModel');
-
-    function getMyModels(req, res) {
-
-        MyModel
-            .find()
-            .sort({
-                date: 'desc'
-            })
-            .exec(function (err, data) {
-                if (err) {
-                    return console.error(err);
-                }
-                res.send(data);
-            })
-        ;
-    }
-
-    function postMyModel(req, res) {
-
-        var mymodel = new MyModel();
-
-        if (req.body.attribute) {
-            mymodel.attribute = req.body.attribute;
-        }
-        else {
-            mymodel.attribute = 'default attribute';
-        }
-
-        if (req.body.attribute2) {
-            mymodel.attribute2 = req.body.attribute2;
-        }
-        else {
-            mymodel.attribute2 = -1;
-        }
-
-        mymodel.date = new Date();
-
-        mymodel.save(function (err) {
-            if (err) {
-                return console.error(err);
-            }
-            res.send(req.body);
-        });
-    }
-
-    server.app.get('/mymodels', server.authenticateMiddleware, getMyModels);
-    server.app.post('/mymodels', server.authenticateMiddleware, postMyModel);
+    server.app.get('/mymodels', server.authenticateMiddleware, authData.getMyModels);
+    server.app.post('/mymodels', server.authenticateMiddleware, authData.postMyModel);
 
 }
 
