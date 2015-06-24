16ee552421594ee00ed91f5affc2abc226ea27aa
dorian-marchal <marchal.dorian@gmail.com>
2015-06-24 17:53:15 +0200

    Add a default gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@
 var gulp  = require('gulp');
 var nodemon  = require('gulp-nodemon');
 
+gulp.task('default', ['start']);
+
 gulp.task('start', function () {
     nodemon({
         script: 'server/server.js',
